981c103d87c47b9b7e7973bfc99704ff75f35d4e
FFlip <none@example.com>
2025-02-12 22:01:14 +1100

    Update

--- edu/edu.js
+++ edu/edu.js
@@ -1,6 +1,16 @@
-const themeToggle = document.getElementById('themeToggle');
-const body = document.body;
-function toggleTheme() {
-  body.classList.toggle('root2');
-}
-themeToggle.addEventListener('click', toggleTheme);
+//  Theme Toggle
+    const themeToggle = document.getElementById('themeToggle');
+    const body = document.body;
+    function toggleTheme() {
+    body.classList.toggle('root2');
+    }
+    themeToggle.addEventListener('click', toggleTheme);
+
+//  Screen Support
+    function showPopup() {
+        document.getElementById("screenSupport").style.display = "block";
+    }
+    
+    function closePopup() {
+        document.getElementById("screenSupport").style.display = "none";
+    }
