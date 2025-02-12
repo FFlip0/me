245e63a81108560e1042ae4a9ad04e9c4e7ec18e
FFlip <none@example.com>
2025-02-12 19:05:59 +1100

    Update

--- edu/edu.js
+++ edu/edu.js
@@ -1,14 +1,6 @@
-
-
-
-    // Get the button and body element
-    const themeToggle = document.getElementById('themeToggle');
-    const body = document.body;
-
-    // Function to toggle the theme
-    function toggleTheme() {
-      body.classList.toggle('root2');
-    }
-
-    // Add event listener to the button
-    themeToggle.addEventListener('click', toggleTheme);
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+function toggleTheme() {
+  body.classList.toggle('root2');
+}
+themeToggle.addEventListener('click', toggleTheme);
