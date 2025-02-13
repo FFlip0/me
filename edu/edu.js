2b7d9856f08b45ec08878d77bfa8ddf8d6fd0bac
FFlip <none@example.com>
2025-02-13 13:02:08 +1100

    Update

--- edu/edu.js
+++ edu/edu.js
@@ -1,16 +1,40 @@
-//  Theme Toggle
-    const themeToggle = document.getElementById('themeToggle');
+//  Theme Toggle Old
+/*
+  const themeToggle = document.getElementById('themeToggle');
     const body = document.body;
     function toggleTheme() {
     body.classList.toggle('root2');
     }
     themeToggle.addEventListener('click', toggleTheme);
+*/
 
-//  Screen Support
+//  Screen Support (with cache saved)
     function showPopup() {
         document.getElementById("screenSupport").style.display = "block";
     }
     
     function closePopup() {
         document.getElementById("screenSupport").style.display = "none";
+    }
+
+
+//Theme Toggle New
+    const themeToggle = document.getElementById('themeToggle');
+    const body = document.body;
+    const themeKey = 'theme'; 
+
+    const savedTheme = localStorage.getItem(themeKey);
+    if (savedTheme === 'dark') {
+    body.classList.add('root2');
+    }
+
+    function toggleTheme() {
+    body.classList.toggle('root2');
+    localStorage.setItem(themeKey, body.classList.contains('root2') ? 'dark' : 'light'); 
+    }
+
+    themeToggle.addEventListener('click', toggleTheme);
+
+    function closePopup() {
+    document.getElementById("screenSupport").style.display = "none";
     }
