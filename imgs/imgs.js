99f79cf2d5c9eed48c2dd3624f81d8cdf4c87820
FFlip0 <niko1482242@gmail.com>
2025-05-19 00:08:19 +1000

    Update

--- imgs/imgs.js
+++ imgs/imgs.js
@@ -5,10 +5,10 @@ const focusedImg = document.getElementById('focusedImg');
 images.forEach(img => {
   img.addEventListener('click', () => {
     focusedImg.src = img.src;
-    overlay.style.display = 'flex'; // Show the overlay
+    overlay.style.display = 'flex';
   });
 });
 
 overlay.addEventListener('click', () => {
-  overlay.style.display = 'none'; // Hide the overlay when clicked
+  overlay.style.display = 'none';
 });
